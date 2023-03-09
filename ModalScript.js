d8bce904746ae3c4b7a60d4222325b120d3eee78
BRCordrey <cordrey.brandon2@gmail.com>
2023-03-09 15:59:38 -0500

    Name Title and background color

---
+++ ModalScript.js
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+// Get the modal
+var car_modal = document.getElementById("SelfDrivingCarModal");
+var robot_modal = document.getElementById("RobotWithGrabberModal");
+
+var car_btn = document.getElementById("SelfDrivingCarBtn");
+var robot_btn = document.getElementById("RobotWithGrabberBtn");
+
+// When the user clicks on the button, open the modal
+car_btn.onclick = function() {
+  car_modal.style.display = "block";
+}
+
+robot_btn.onclick = function() {
+  robot_modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == car_modal || event.target == robot_modal ) {
+    event.target.style.display = "none";
+  }
+}
+=======
+// Get the modal
+var car_modal = document.getElementById("SelfDrivingCarModal");
+var robot_modal = document.getElementById("RobotWithGrabberModal");
+
+var car_btn = document.getElementById("SelfDrivingCarBtn");
+var robot_btn = document.getElementById("RobotWithGrabberBtn");
+
+// When the user clicks on the button, open the modal
+car_btn.onclick = function() {
+  car_modal.style.display = "block";
+}
+
+robot_btn.onclick = function() {
+  robot_modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == car_modal || event.target == robot_modal ) {
+    event.target.style.display = "none";
+  }
+}
+>>>>>>> ffdffbf7eab86f2cf1d6a5cc6d20a0d5336f75e9
